c498fad1884f77a2847bc636961b516a32e7e4c0
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2024-03-21 21:02:16 +1300

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-nthlink.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-chrome.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-warpplus.zip
