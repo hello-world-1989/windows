ba174757138bb9543e00c496c1458792a186add3
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2024-03-29 04:01:48 +0000

    update apps

---
+++ download.sh
@@ -0,0 +1,8 @@
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-nthlink.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-chrome.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-warpplus.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-tor.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-session.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-ooni.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-outline.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-vpngate.zip
