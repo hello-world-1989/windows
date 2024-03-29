01ae8d720c9f1883d9565bd030d2d124b8c3ec7c
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2024-03-29 17:21:50 +1300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-nthlink.zip
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-chrome.zip
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-warpplus.zip
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-tor.zip
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-session.zip
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-ooni.zip
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-outline.zip
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-vpngate.zip
+wget -O windows-nthlink.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-nthlink.zip
+wget -O windows-chrome.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-chrome.zip
+wget -O windows-warpplus.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-warpplus.zip
+wget -O windows-tor.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-tor.zip
+wget -O windows-session.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-session.zip
+wget -O windows-ooni.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-ooni.zip
+wget -O windows-outline.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-outline.zip
+wget -O windows-vpngate.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/windows/windows-vpngate.zip
